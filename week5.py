611a932eb378a09db09cb28a23fec521847f1f35
Kirti Sachdeva <sachdevakirti637@gmail.com>
2024-09-27 12:41:59 +0530

    file updated

--- week5.py
+++ week5.py
@@ -1,3 +1,12 @@
+'''
+Objective: Set up and understand FAST API for creating a web-based API.
+Tasks:
+Install and configure FAST API.
+Create basic endpoints for adding and retrieving records.
+Test API endpoints using tools like Postman.
+
+'''
+
 from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 
@@ -15,16 +24,17 @@ my_posts = [{"name":"kirti","email":"kirti@gmail.com","rollno":1}]
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/posts")
 def get_posts():
     return {"data":my_posts}
+
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     my_posts.append(post_dict)
     return {"data":post_dict}
-
 
 def find_post(rollno):
     for p in my_posts:
@@ -36,6 +46,7 @@ def find_index_post(rollno):
         if p['rollno'] == int(rollno):
             return i
         
+
 @app.get("/posts/{rollno}")
 def get_post(rollno: int):
     post = find_post(rollno)
@@ -45,6 +56,7 @@ def get_post(rollno: int):
         #return {"message": f"post with id: {id} was not found"}
     return {"post-details": post}
 
+
 @app.delete("/posts/{rollno}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_student(rollno: int):
     index = find_index_post(rollno)
@@ -53,6 +65,7 @@ def delete_student(rollno: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Student with rollno {rollno} does not exist")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/posts/{rollno}")
 def update_record(rollno: int, post: Post):
